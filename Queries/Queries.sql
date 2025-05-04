87425b9e611777c0d6c82d8a082bfa6474df772d
Saman Esmaeili <samanesmaeili01@gmail.com>
2025-05-04 20:45:49 +0330

    1 to 5 queries been added

--- Queries/Queries.sql
+++ Queries/Queries.sql
@@ -5,8 +5,71 @@ WHERE UserID NOT IN(
 );
 
 SELECT DISTINCT u.FirstName, u.LastName
-FROM user u
-JOIN reservation r ON u.UserID = r.UserID
-JOIN payment p ON r.ReservationID = p.ReservationID
-WHERE p.PaymentStatus = 'موفق';
+FROM 
+    user u
+JOIN 
+    reservation r ON u.UserID = r.UserID
+WHERE
+    r.ReservationStatus = 'تایید شده'
+
+
+SELECT 
+    u.UserID,
+    u.FirstName,
+    u.LastName,
+    u.Email,
+    YEAR(p.PaymentTime) AS Year,
+    MONTH(p.PaymentTime) AS Month,
+    SUM(t.Price) AS TotalPayments
+FROM 
+    User u
+JOIN 
+    Payment p ON u.UserID = p.UserID
+JOIN 
+    Reservation r ON p.ReservationID = r.ReservationID
+JOIN 
+    Ticket t ON r.TicketID = t.TicketID
+WHERE 
+    r.ReservationStatus = 'تایید شده'
+GROUP BY 
+    u.UserID, u.FirstName, u.LastName, u.Email, YEAR(p.PaymentTime), MONTH(p.PaymentTime)
+ORDER BY 
+    u.UserID, Year, Month;
+
+SELECT
+    u.UserID,
+    u.FirstName,
+    u.LastName,
+    c.CityName
+FROM
+    User u
+JOIN
+    Reservation r ON u.UserID = r.UserID 
+JOIN
+    city c ON u.CityID = c.CityID
+WHERE
+    r.ReservationStatus = 'تایید شده' 
+GROUP BY 
+    u.FirstName, u.LastName, c.CityName
+HAVING
+    COUNT(r.ReservationID) = 1;
+
+SELECT 
+    u.FirstName,
+    u.LastName,
+    u.Email,
+    u.PhoneNumber
+FROM
+    User u
+JOIN
+    Reservation r ON u.UserID = r.UserID
+WHERE
+    r.ReservationStatus = (
+        SELECT MAX(ReservationStatus) 
+        FROM Reservation 
+        WHERE UserID = u.UserID 
+    )
+ORDER BY
+    r.`ReservationTime` DESC
+LIMIT 1;
 
